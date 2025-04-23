1bcbf3ed0e5bcb06058cdac8a098edc95c98e287
Aashish-gif <Aashishtejwani12@gmail.com>
2025-04-23 18:26:56 +0530

    Added MCP Server

---
+++ MCP/index.js
@@ -0,0 +1,38 @@
+import express from "express";
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { SSEServerTransport } from "@modelcontextprotocol/sdk/server/sse.js";
+
+const server = new McpServer({
+  name: "example-server",
+  version: "1.0.0"
+});
+
+// ... set up server resources, tools, and prompts ...
+
+const app = express();
+
+
+const transports= {};
+
+app.get("/sse", async (req , res ) => {
+  const transport = new SSEServerTransport('/messages', res);
+  transports[transport.sessionId] = transport;
+  res.on("close", () => {
+    delete transports[transport.sessionId];
+  });
+  await server.connect(transport);
+});
+
+app.post("/messages", async (req , res ) => {
+  const sessionId = req.query.sessionId ;
+  const transport = transports[sessionId];
+  if (transport) {
+    await transport.handlePostMessage(req, res);
+  } else {
+    res.status(400).send('No transport found for sessionId');
+  }
+});
+
+app.listen(3001 , () => {
+    console.log("Sever s running on http://localhost:3001");
+});
